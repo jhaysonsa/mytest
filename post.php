7c1d397398c4120d174ad33d8ee469332af68ad4
jhaysonsa <sonahj00@gmail.com>
2017-04-06 15:56:57 +0800

    add existing files

---
+++ post.php
@@ -0,0 +1,63 @@
+<?php
+/*
+   $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   $name=$_POST['name'];
+   $email=$_POST['email'];
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   $sql = "INSERT INTO person ".
+      "(Name,Email) ".
+      "VALUES ( '".$name."','".$email."' )";
+      
+   mysql_select_db('test');
+   $retval = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not enter data: ' . mysql_error());
+   }
+   
+   echo "Entered data successfully\n";
+   
+   mysql_close($conn);*/
+
+
+if($_POST["name"])
+{
+$name = $_POST["name"];
+$email = $_POST["email"];
+// Here, you can also perform some database query operations with above values.
+    $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   $name=$_POST['name'];
+   $email=$_POST['email'];
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   $sql = "INSERT INTO person ".
+      "(Name,Email) ".
+      "VALUES ( '".$name."','".$email."' )";
+      
+   mysql_select_db('test');
+   $retval = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not enter data: ' . mysql_error());
+   }
+   
+   echo "Entered data successfully\n";
+   
+   mysql_close($conn);
+echo "Welcome ". $name ."!"; // Success Message
+}
+
+?>
